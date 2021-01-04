d1d599309a81b7d1e7e2b630f54d44394ba38719
ChengHaoKe <102256006c@gmail.com>
2021-01-03 18:42:48 -0600

    updates

--- flaskdemo/app/crawlerclass.py
+++ flaskdemo/app/crawlerclass.py
@@ -4,20 +4,29 @@ from os.path import expanduser as ep
 # import requests
 
 
-def firefox(dr):
-    firefox_driver = ep('~') + dr
+class webcrawler:
+    def __init__(self, driver):
+        self.dr = ep('~') + driver
 
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference("browser.privatebrowsing.autostart", True)
-    profile.set_preference("browser.download.folderList", 2)
-    profile.set_preference("browser.download.manager.showWhenStarting", False)
-    profile.update_preferences()
-    driver = webdriver.Firefox(executable_path=firefox_driver, firefox_profile=profile)
-    # driver.set_window_size(1390, 850)
-    driver.maximize_window()
+    def firefox(self):
+        firefox_driver = self.dr
 
-    return driver
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("browser.privatebrowsing.autostart", True)
+        profile.set_preference("browser.download.folderList", 2)
+        profile.set_preference("browser.download.manager.showWhenStarting", False)
+        profile.update_preferences()
+        driver = webdriver.Firefox(executable_path=firefox_driver, firefox_profile=profile)
+        # driver.set_window_size(1390, 850)
+        driver.maximize_window()
+
+        return driver
+
+    def crawler(self):
+        driver = self.firefox()
+        driver.get('https://chenghaoke.shinyapps.io/rshiny/')
 
 
 if __name__ == '__main__':
+
     pass
